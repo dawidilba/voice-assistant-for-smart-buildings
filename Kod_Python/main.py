06e698361ce4a76ddf6203666b3fe8acc11a3109
dawidilba <dawidilba@gmail.com>
2021-06-07 23:43:30 +0200

    Need to fix keywords list

--- Kod_Python/main.py
+++ Kod_Python/main.py
@@ -12,9 +12,11 @@ class VoiceAssistant:
         self.dht = adafruit_dht.DHT22(board.D13, use_pulseio=False)
         self.leds = apa102.APA102(num_led=3, global_brightness=0b1)
         self.speech = LiveSpeech(
-            verbose=False,
+            kws = '/home/pi/voice-assistant-for-smart-buildings/Kod_Python/key.list',
+            lm=False,
+            verbose=True,
             sampling_rate=16000,
-            buffer_size=2048,
+            buffer_size=1048,
             no_search=False,
             full_utt=False,
             )
@@ -23,9 +25,23 @@ class VoiceAssistant:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(12, GPIO.OUT)
         
-
     def __del__(self):
         GPIO.cleanup()
+
+    def getSensorData(self):
+        temp = 0
+        humidity = 0
+        try:
+            temp = self.dht.temperature
+            humidity = self.dht.humidity
+        except RuntimeError as err:
+            print(err.args[0])
+        except Exception as err:
+            self.dht.exit()
+            raise err
+        if(temp != None and humidity != None):
+            self.temp = temp
+            self.humidity = humidity
 
     def turnOnLed(self):
         GPIO.output(12, GPIO.HIGH)
@@ -41,40 +57,38 @@ class VoiceAssistant:
     def turnOffRespeakerLeds(self):
         self.leds.clear_strip()
 
-    def viewSensorInfoOnLcd(self):
+    def viewTempOnLcd(self):
         self.lcd.clear()
-        result = None
-        while result is None:
-            try:
-                self.temp = self.dht.temperature
-                self.humidity = self.dht.humidity
-            except:
-                pass
+        self.getSensorData()
         self.lcd.text("Temp: {:.1f} C".format(self.temp), 1)
         self.lcd.text("Humidity: {}% ".format(self.humidity), 2)
         # print("Temp: {:.1f} C Humidity: {}% ".format(self.temp, self.humidity))
     
     def action(self, words):
-        if("show temperature"):
-            self.viewSensorInfoOnLcd()
-        if("lights on"):
-            self.turnOnLed()
+        tmp = str(words)
+        if("show temperature" in tmp or "show humidity" in tmp):
+            self.viewTempOnLcd()
+            print("tu")
+        if("lights on" in tmp):
             self.turnOnRespeakerLeds()
-            print("done")
-        if("lights off"):
-            self.turnOffLed()
+        if("lights off" in tmp):
             self.turnOffRespeakerLeds()
-        if("light color to red"):
+        if("lamp on" in tmp):
+            self.turnOnLed()
+        if("lamp off" in tmp):
+            self.turnOffLed()
+        if("set light color to red" in tmp):
             self.turnOnRespeakerLeds(color=[255,0,0])
-        if("light color to green"):
+        if("set light color to green" in tmp):
             self.turnOnRespeakerLeds(color=[0,255,0])
-        if("light color to blue"):
+        if("set light color to blue" in tmp):
             self.turnOnRespeakerLeds(color=[0,0,255]) 
 
     def startRecognition(self):
         for phrase in self.speech:
             self.lcd.clear()
             print(phrase)
+            self.lcd.text(str(phrase),1)
             self.action(phrase)
     
 va = VoiceAssistant()
